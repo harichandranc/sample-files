83245ffedf8af36907992147ab75ca844d803479
Harichandran C <harichstech@gmail.com>
2026-06-10 00:31:20 +0530

    updated service page

--- src/pages/WordFiles.jsx
+++ src/pages/WordFiles.jsx
@@ -5,17 +5,17 @@ function WordFiles() {
   {
     name: "Sample DOCX (100 KB)",
     size: "100 KB",
-    path: "/word/sample-100kb.docx",
+    path: "/word/sample100kb.docx",
   },
   {
     name: "Sample DOCX (500 KB)",
     size: "500 KB",
-    path: "/word/sample-500kb.docx",
+    path: "/word/sample500kb.docx",
   },
   {
     name: "Sample DOCX (1.5 MB)",
     size: "1.5 MB",
-    path: "/word/sample-1-5mb.docx",
+    path: "/word/sample1.5mb.docx",
   },
 ];
 
