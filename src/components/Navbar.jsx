075c24bb2b67edc990e7e1b5e5ec203ad2fd08e4
Harichandran C <harichstech@gmail.com>
2026-06-14 11:55:12 +0530

    Update project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "../styles/layout.css";
 import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav-container">
@@ -9,16 +12,53 @@ function Navbar() {
           Sample Files
         </Link>
 
-        <div className="nav-links">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/pdf-files">PDF</NavLink>
-          <NavLink to="/word-files">Word</NavLink>
-          <NavLink to="/excel-files">Excel</NavLink>
-          <NavLink to="/csv-files">CSV</NavLink>
-          <NavLink to="/image-files">Images</NavLink>
-          <NavLink to="/audio-files">Audio</NavLink>
-          <NavLink to="/video-files">Video</NavLink>
-          <NavLink to="/ppt-files">PowerPoint</NavLink>
+        <button
+          className="menu-btn"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </button>
+
+        <div
+          className={`nav-links ${
+            menuOpen ? "active" : ""
+          }`}
+        >
+          <NavLink to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </NavLink>
+
+          <NavLink to="/pdf-files" onClick={() => setMenuOpen(false)}>
+            PDF
+          </NavLink>
+
+          <NavLink to="/word-files" onClick={() => setMenuOpen(false)}>
+            Word
+          </NavLink>
+
+          <NavLink to="/excel-files" onClick={() => setMenuOpen(false)}>
+            Excel
+          </NavLink>
+
+          <NavLink to="/csv-files" onClick={() => setMenuOpen(false)}>
+            CSV
+          </NavLink>
+
+          <NavLink to="/image-files" onClick={() => setMenuOpen(false)}>
+            Images
+          </NavLink>
+
+          <NavLink to="/audio-files" onClick={() => setMenuOpen(false)}>
+            Audio
+          </NavLink>
+
+          <NavLink to="/video-files" onClick={() => setMenuOpen(false)}>
+            Video
+          </NavLink>
+
+          <NavLink to="/ppt-files" onClick={() => setMenuOpen(false)}>
+            PowerPoint
+          </NavLink>
         </div>
       </div>
     </nav>
